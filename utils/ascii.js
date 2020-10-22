76952444007e0edc692d3b46b49543da04ad4ac0
Daniel <daniel.sdm.github@gmail.com>
2020-10-22 12:40:39 -0300

    🔖 release: Discor v1.1.0

--- utils/ascii.js
+++ utils/ascii.js
@@ -4,8 +4,8 @@ const discorAscii = `
     (O O)
      \=/
     .-"-.
-   //\ /\\               Discor v1.0.1
- _// / \ \\_      Fast discordjs bot creation
+   //\ /\\               Discor v1.1.0
+ _// / \ \\_        Fast discord bot creation
 =./ {,-.} \.=
     || ||
     || ||    
