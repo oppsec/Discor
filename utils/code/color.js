f2979e36037dd25799ef7056291e304fc5f63ed5
Daniel <daniel.sdm.github@gmail.com>
2020-10-26 15:27:06 -0300

    🔥 delete: old code path

--- utils/code/color.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-
-const green = chalk.greenBright;
-const blue = chalk.blueBright;
-const yellow = chalk.yellowBright;
-const red = chalk.redBright;
-
-module.exports = { green, blue, yellow, red };
