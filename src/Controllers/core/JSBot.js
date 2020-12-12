3308f336bb08e28d72741f23501984bc716e7996
Arthur Tavares <arthurabreu00@gmail.com>
2020-12-12 03:56:24 -0300

    Refactoring for implementing the template method

--- src/Controllers/core/JSBot.js
+++ src/Controllers/core/JSBot.js
@@ -1,6 +1,14 @@
-module.exports = function createJSBot(myBot) 
-{
-    return `
+const Generator = require("./Generator.js");
+const { exec } = require('child_process')
+
+module.exports = class JSBot extends Generator {
+ 
+    /**
+     * @override
+     * @param {object} bot 
+     */
+    fetchData(bot) {
+            return `
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
 
@@ -8,8 +16,8 @@ dotenv.config();
 const client = new Discord.Client();
         
 client.on('ready', () => {
-    console.log('- ${myBot.botName} online');
-    console.log('- Version: ${myBot.botVersion} | Description: ${myBot.botDesc}');
+    console.log('- ${bot.name} online');
+    console.log('- Version: ${bot.version} | Description: ${bot.description}');
 });
         
 client.on('message', msg => {
@@ -20,4 +28,13 @@ client.on('message', msg => {
         
 client.login(process.env.TOKEN);
 `;
+    }
+
+    /**
+     * @override
+     */
+    installLibraries(directory){
+        process.chdir(directory);
+        exec(`npm i discord.js`);
+    }
 }
